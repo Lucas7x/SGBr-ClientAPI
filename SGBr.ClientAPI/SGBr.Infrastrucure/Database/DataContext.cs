30a4ea0453a9afb1def84ab4c7bc99f767f03d50
Lucas Xavier <lukasx1934@gmail.com>
2026-02-05 11:33:04 -0300

    feat: criando DataContext

---
+++ SGBr.ClientAPI/SGBr.Infrastrucure/Database/DataContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SGBr.Domain.Entities;
+
+namespace SGBr.Infrastrucure.Database
+{
+    public class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
+    {
+        public DbSet<Client> Clients { get; set; }
+    }
+}
