45c7c1149ad71418fbeac56cf0fb4fed34e4d8b8
Lucas Xavier <lukasx1934@gmail.com>
2026-02-05 12:11:47 -0300

    feat: configurando injeção de dependências

---
+++ SGBr.ClientAPI/SGBr.API/Extensions/BuilderServiceExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SGBr.Infrastrucure.Database;
+
+namespace SGBr.API.Extensions
+{
+    public static class BuilderServiceExtensions
+    {
+        public static IServiceCollection ConfigureDatabaseContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            string? defaultConnection = configuration.GetConnectionString("DefaultConnection");
+
+            if (string.IsNullOrWhiteSpace(defaultConnection))
+                throw new ArgumentNullException(nameof(defaultConnection));
+
+            services.AddDbContext<DataContext>(
+                options => options.UseSqlite(defaultConnection)
+            );
+
+            return services;
+        }
+    }
+}
--- SGBr.ClientAPI/SGBr.API/Program.cs
+++ SGBr.ClientAPI/SGBr.API/Program.cs
@@ -1,3 +1,7 @@
+using SGBr.API.Extensions;
+using SGBr.Application.Interfaces;
+using SGBr.Infrastrucure.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Configure DataContext
+builder.Services.ConfigureDatabaseContext(builder.Configuration);
+
+// Registering Services and Repositories
+builder.Services.AddScoped<IClientRepository, ClientRepository>();
+builder.Services.AddScoped<IClientService, IClientService>();
 
 var app = builder.Build();
 
