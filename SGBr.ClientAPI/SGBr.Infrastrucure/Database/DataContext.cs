cc5b5b68c8f3dacfb69e8db7f8bd78446b0a685f
Lucas Xavier <lukasx1934@gmail.com>
2026-02-05 13:06:49 -0300

    feat: criando  migration inicial: criação da tabela tabela Client

--- SGBr.ClientAPI/SGBr.Infrastrucure/Database/DataContext.cs
+++ SGBr.ClientAPI/SGBr.Infrastrucure/Database/DataContext.cs
@@ -6,5 +6,14 @@ namespace SGBr.Infrastrucure.Database
     public class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
     {
         public DbSet<Client> Clients { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Client>()
+                .HasIndex(c => c.Email)
+                .IsUnique();
+        }
     }
 }
---
+++ SGBr.ClientAPI/SGBr.Infrastrucure/Migrations/20260205155748_InitialMigration.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SGBr.Infrastrucure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clients_Email",
+                table: "Clients",
+                column: "Email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clients");
+        }
+    }
+}
